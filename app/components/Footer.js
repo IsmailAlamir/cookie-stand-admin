ee9d18fb79067f06448722b84e770d39d01a55c6
IsmailAlamir <ismaeelalamir@gmail.com>
2022-12-31 22:16:07 +0300

    form

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-4 text-center">
+      Copyright (c) 2023 Ismail Al Amir
+      <br />
+      All rights reserved.
+    </footer>
+  )
+}
